9dde13ef94d3dde752a072d222ce6e24567559ae
Reimos7 <tjrvud9@gmail.com>
2024-05-22 17:19:17 +0900

    guard let binding (alert 숫자로만) 대응 완료

--- bmiCalculator/bmiCalculatorViewController.swift
+++ bmiCalculator/bmiCalculatorViewController.swift
@@ -56,31 +56,16 @@ class bmiCalculatorViewController: UIViewController {
    
     // 결과 확인 버튼
     // alert를 사용 -> bmi 결과 보여줌 (취소 버튼 1개)
-    @IBAction func resultButtonClicked(_ sender: UIButton) {
+    @IBAction func resultButtonClicked(_ sender: UIButton){
         
-        
-        // 1) alert창 제목, 메세지 설정
-        let alert = UIAlertController(title: "BMI 결과", message: "\(bmiCalculate())", preferredStyle: .alert)
-        
-        // 2) 버튼 생성
-        let cancel = UIAlertAction(title: "취소", style: .cancel)
-        
-        // 3) 버튼 생성
-        alert.addAction(cancel)
-        
-        // 4) 버튼 보여주기
-        present(alert, animated: true)
-        
-    }
-    // Double로 리턴
-    func bmiCalculate () -> String {
-        
+        var bmi = 0.0
         var bmiResult: String
-        var height = Double(bodyHeightTextField.text!)!
-        var weight = Double(bodyWeightTextField.text!)!
         
-        let bmi = weight / (height * height * 0.0001)
-       
+        guard let height = Double(bodyHeightTextField.text!) , let weight = Double(bodyWeightTextField.text!) else {
+            return alertMessage("경고","숫자만 입력해주세요")
+        }
+        bmi = weight / (height * height * 0.0001)
+        
         switch bmi {
         case 0.0 ..< 18.5:
             bmiResult = " 저체중"
@@ -97,10 +82,16 @@ class bmiCalculatorViewController: UIViewController {
         }
         // bmi 소수점 반올림
         bmiResult = String(round(bmi)) + bmiResult
-        return bmiResult
+        alertMessage("BMI결과",bmiResult)
+        
     }
     
+    @IBAction func randomButtonClicked(_ sender: UIButton)  {
+        alertMessage("랜덤 BMI",randomBmiCalculator())
+       
+    }
     func randomBmiCalculator () -> String  {
+        
         var randomBmiResult: String
         // 키는 160이상 180이하
         let randomHeight = Double.random(in: 160...180)
@@ -129,9 +120,11 @@ class bmiCalculatorViewController: UIViewController {
         
     }
   
-    @IBAction func randomButtonClicked(_ sender: UIButton)  {
+    // alert 설정 메시지 
+    func alertMessage(_ mainTitleMessage: String,_ subTitleMessage: String){
+        
         // 1) alert창 제목, 메세지 설정
-        let alert = UIAlertController(title: "BMI 결과", message: "\(randomBmiCalculator())", preferredStyle: .alert)
+        let alert = UIAlertController(title: mainTitleMessage, message: "\(subTitleMessage)", preferredStyle: .alert)
         
         // 2) 버튼 생성
         let cancel = UIAlertAction(title: "취소", style: .cancel)
@@ -141,6 +134,7 @@ class bmiCalculatorViewController: UIViewController {
         
         // 4) 버튼 보여주기
         present(alert, animated: true)
+        
     }
    
     // 텍스트 필드 설정
@@ -150,11 +144,57 @@ class bmiCalculatorViewController: UIViewController {
         textField.layer.borderColor = UIColor.systemGray.cgColor
     }
     
-    
     // 제스처로 키보드 숨기기
     @IBAction func keyBoardDissmiss(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
     }
+    
+    
+    
+    // Double로 리턴
+//    func bmiCalculate () -> String {
+//
+//        var bmi = 0.0
+//        var bmiResult: String
+//
+//        guard let height = bodyHeightTextField.text , let weight = bodyWeightTextField.text else{
+//            alertMessage(bmiCalculate())
+//        }
+//
+//        if let height = Double(bodyHeightTextField.text!), let weight = Double(bodyWeightTextField.text!){
+//
+//            bmi = weight / (height * height * 0.0001)
+//
+//            // 문자열 입력 받았을때
+//        } else if bodyWeightTextField.text == "" && bodyHeightTextField.text == "" {
+//            // alert로 대응
+//
+//        } else {
+//
+//        }
+//
+        //var height = Double(bodyHeightTextField.text!)!
+       // var weight = Double(bodyWeightTextField.text!)!
+       
+//        switch bmi {
+//        case 0.0 ..< 18.5:
+//            bmiResult = " 저체중"
+//        case 18.5 ..< 25.0:
+//            bmiResult = " 정상체중"
+//        case 25.0 ..< 30.0:
+//            bmiResult = " 비만 전단계"
+//        case 30.0 ..< 35.0:
+//            bmiResult = " 경도 비만"
+//        case 35.0 ..< 40.0:
+//            bmiResult = " 중등도 비만"
+//        default: // 40 이상
+//            bmiResult = " 고도 비만"
+//        }
+//        // bmi 소수점 반올림
+//        bmiResult = String(round(bmi)) + bmiResult
+//        return bmiResult
+//    }
+//
     
     // 함수 매개변수로 alert 함수 받기 포기 ..
 //    func bmiResultAlert (_ bmiMessage: ()) {
